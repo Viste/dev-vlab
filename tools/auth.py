55c3fb61e0989124dbe53163bda961bae36bb533
viste <viste02@gmail.com>
2024-08-20 19:16:04 +0300

    update auth
    Signed-off-by: viste <viste02@gmail.com>

--- tools/auth.py
+++ tools/auth.py
@@ -27,11 +27,21 @@ def authenticate_user(username, password):
 
 def authenticate_vk_user(vk_id, screen_name, first_name, last_name, profile_picture, email):
     user = User.query.filter_by(vk_id=vk_id).first()
+
     if not user:
+        current_app.logger.debug(f"Creating a new VK user with ID: {vk_id}")
         user = User(username=screen_name, vk_id=vk_id, first_name=first_name, last_name=last_name,
                     profile_picture=profile_picture, email=email, provider='vk')
         db.session.add(user)
-        db.session.commit()
+
+        try:
+            db.session.commit()  # Фиксируем транзакцию
+            current_app.logger.debug(f"New VK user created and committed to the database with ID: {user.id}")
+        except Exception as e:
+            current_app.logger.error(f"Failed to commit new VK user to the database. Error: {e}")
+            db.session.rollback()  # Откатываем транзакцию в случае ошибки
+            return False
+
     session['loggedin'] = True
     session['id'] = user.id
     session['username'] = user.username
