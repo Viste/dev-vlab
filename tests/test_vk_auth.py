760e7c1194c17306bdb479382b443f0742024427
viste <viste02@gmail.com>
2024-08-20 14:39:18 +0300

    update tests
    Signed-off-by: viste <viste02@gmail.com>

--- tests/test_vk_auth.py
+++ tests/test_vk_auth.py
@@ -37,7 +37,7 @@ def test_authorize_vk_success(mock_setup_routes, client):
     with patch('tools.auth.authenticate_vk_user') as mock_authenticate:
         response = client.get('/vk/callback')
         assert response.status_code == 302  # Redirects after successful login
-        assert mock_authenticate.called_once_with(
+        mock_authenticate.assert_called_once_with(
             12345, 'john_doe', 'John', 'Doe', 'https://i.pinimg.com/736x/90/bd/09/90bd09bc51dba0bd127db954591d3650.jpg', 'user@example.com'
         )
 
@@ -54,7 +54,7 @@ def test_authorize_vk_no_token(mock_setup_routes, client):
     response = client.get('/vk/callback')
 
     assert response.status_code == 302
-    assert b'login' in response.location
+    assert 'login' in response.location
 
 
 @patch('app.setup_routes')
@@ -73,4 +73,4 @@ def test_authorize_vk_profile_failure(mock_setup_routes, client):
     response = client.get('/vk/callback')
 
     assert response.status_code == 302
-    assert b'login' in response.location
+    assert 'login' in response.location
