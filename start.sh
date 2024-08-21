584a9939c931664885a8d32b21be60a792cbd531
viste <viste02@gmail.com>
2024-08-21 21:06:24 +0300

    update
    Signed-off-by: viste <viste02@gmail.com>

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ echo "Running database migrations..."
 flask db upgrade
 
 echo "Running App gunicorn server..."
-exec gunicorn -b 0.0.0.0:5000 -w 32 main:app
+exec gunicorn -b 0.0.0.0:8000 -w 24 app:app
