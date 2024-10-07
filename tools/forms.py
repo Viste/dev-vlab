93131a96e51c0fd39b9906ba3a3e86c7118330fb
viste <viste02@gmail.com>
2024-10-08 00:26:46 +0300

    fix after part1
    Signed-off-by: viste <viste02@gmail.com>

--- tools/forms.py
+++ tools/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from werkzeug.security import check_password_hash
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField, FileField
 from wtforms import validators, fields, form
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 
